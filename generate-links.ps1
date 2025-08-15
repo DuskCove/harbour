bf8580447e8f6bf1e9448a9e1cc99a3ff817b893
DuskCove <duskcove@gmail.com>
2025-08-15 10:34:52 +0100

    add index.

---
+++ generate-links.ps1
@@ -0,0 +1,19 @@
+$baseUrl = "https://github.com/DuskCove/harbour/blob/main/"
+$outputFile = "links.md"
+
+Remove-Item $outputFile -ErrorAction Ignore
+
+foreach ($dir in "inventory", "blueprints") {
+    Add-Content $outputFile "## $dir"
+    Add-Content $outputFile ""
+
+    Get-ChildItem -Path ".\$dir" -Recurse -File |
+        Sort-Object Name |
+        ForEach-Object {
+            Add-Content $outputFile ("[{0}]({1}{2})" -f $_.Name,
+                $baseUrl,
+                ($_.FullName.Substring((Get-Location).Path.Length + 1) -replace "\\","/")
+            )
+            Add-Content $outputFile ""  # Blank line between rows
+        }
+}
